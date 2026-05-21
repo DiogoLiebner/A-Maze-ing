4cdf9a041eb5ecd72ce1005f4e8e6a0874c0ea68
DiogoLiebner <diogoliebner@gmail.com>
2026-05-21 18:22:58 +0100

    started doing pathfinding algo and cleared some flake8 and mypy errosin other files

--- srcs/maze_generation.py
+++ srcs/maze_generation.py
@@ -39,9 +39,8 @@ def _carve_passages(
         for dr, dc in directions:
             nr, nc = current_r + dr, current_c + dc
 
-            if (0 <= nr < height) and (0 <= nc < width)
-                and (nr, nc) not in visited:
-                
+            if (0 <= nr < height) and (0 <= nc < width) and (nr, nc) not in visited:
+
                 grid[2 * nr + 1][2 * nc + 1] = 0
 
                 grid [2 * current_r + 1 + dr][2 * current_c + 1 + dc] = 0
@@ -60,7 +59,7 @@ def _add_loops(grid: list[list[int]], loop_factor: float) -> None:
         Randomly adds loops to the maze by removing some walls.
     """
     rows: int = len(grid)
-    cols: int= len(grid[0])
+    cols: int = len(grid[0])
 
     candidate_walls: list[tuple[int, int]] = []
 
@@ -81,12 +80,16 @@ def _add_loops(grid: list[list[int]], loop_factor: float) -> None:
 def generate_maze(config: MazeConfig, loop_factor: float = 0.1) -> list[list[int]]:
     width: int = config["width"]
     height: int = config["height"]
+    entry: tuple[int, int] = config["entry"]
+    exit: tuple[int, int] = config["exit"]
     perfect: bool = config["perfect"]
 
     grid: list[list[int]] = _build_grid(width, height)
     visited: set[tuple[int, int]] = set()
 
-    _carve_passages(grid, 0, 0, height, width, visited)
+    _carve_passages(grid, entry[0], entry[1], height, width, visited)
+
+    grid[2 * exit[0] + 1][2 * exit[1] + 1] = 0
 
     if not perfect:
         _add_loops(grid, loop_factor)
