d2c2ca0740f755ba5f3090baa4744c955d75e236
DSandaliasII <107626052+DSandaliasII@users.noreply.github.com>
2026-06-06 16:52:35 +0100

    Header file was Wooooo but now it's Weeeeeee

--- MiniLibX/maze.h
+++ MiniLibX/maze.h
@@ -55,16 +55,15 @@ typedef struct s_data
 
     int     path_progress;
     int     wall_color;
+    char    *filename;
 }   t_data;
 
 
 
 
 void    put_pixel(t_data *data, int x, int y, int color);
-void    draw_something(t_data *data);
 int     key_hook(int keycode, void *param);
 int     close_window(void *param);
-void	draw_circle(t_data *data, int cx, int cy, int r, int color);
 void    render(t_data *data);
 void    render_maze(t_data *data);
 void    draw_line(t_data *data, int x1, int y1, int x2, int y2, int color, int thickness);
@@ -72,9 +71,9 @@ void    parse_maze_file(t_data *data, char *filename);
 void    allocate_grid(t_data *data, int rows, int cols);
 char	**ft_split(char const *s, char c);
 void build_path_cells(t_data *d);
-int is_on_path(t_data *d, int x, int y);
 void fill_cell(t_data *d, int x, int y, int w, int h, int color);
 void draw_path_line(t_data *d);
 int animate(void *param);
+void render_entry_exit(t_data *d);
 
 #endif
